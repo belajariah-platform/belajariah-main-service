4674d4c15c2fe36fb02e224631ee478adf9ffa6d
Heri Heryanto <Heri.Heryanto-EXT@xapiens.id>
2021-04-06 21:33:05 +0700

    add ssl

--- config/database.go
+++ config/database.go
@@ -16,7 +16,7 @@ func ConnectDB(config *model.Config) *sqlx.DB {
 
 func getDBConnection(config *model.Config) *sqlx.DB {
 	dbConnectionStr := fmt.Sprintf(
-		"host=%s port=%d dbname=%s user=%s password=%s sslmode=disable",
+		"host=%s port=%d dbname=%s user=%s password=%s sslmode=enable",
 		config.Database.Host,
 		config.Database.Port,
 		config.Database.DbName,
